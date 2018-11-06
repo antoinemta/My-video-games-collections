1e8f9a79aab75a0e9a46f5101c05cdb8e9540472
antoinemta <antoine_mta@hotmail.fr>
2018-11-06 22:18:50 +0100

    I integred a switch system to display the game posters in line or cards.

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,47 +4,104 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Filter from "./Filter";
 
-const Homepage = () => (
-  <div className="col-12 homePage px-0">
-    <section className="row mx-0">
-      <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true}>
-        <div>
-          <img src="index.jpeg" className="imgCarousel" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src="index.jpeg" className="imgCarousel" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src="index.jpeg" className="imgCarousel" />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
-    </section>
-    <Filter />
-    <section className="row mx-0">
-      <div className="col-12 layoutOrganisation">
-        <div className="col-sm-6 col-xl-10 py-4 border-right">
-          <span className="sectionTitle text-white">
-            <u>Games which may interest you</u>
-          </span>
-        </div>
-        <div className="col d-flex justify-content-center pt-4">
-          <img src="card.png" className="btnList mr-4" />
-          <img src="line.png" className="btnList" />
-        </div>
-      </div>
-    </section>
-    <section className="row mx-0 py-5 mt-4">
-      <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 px-0 my-4 cardGame">
-        <div className="relativeCard">
-          <img src="index.jpeg" className="imgCard" />
-          <span className="titleGame">Metal gear solid V</span>
-        </div>
-      </div>
-    </section>
-  </div>
-);
+class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      classCardGame: "col-xl-2 col-lg-3 col-md-4 col-sm-6 px-0 my-4 cardGame",
+      classRelativeCard: "relativeCard",
+      classImgCard: "imgCard",
+      classTitleGame: "titleGame",
+      listTitleGame: "notDisplay",
+      classRow: "row mx-0 py-5 mt-4"
+    };
+  }
 
+  displayCard() {
+    this.setState({
+      classCardGame: "col-xl-2 col-lg-3 col-md-4 col-sm-6 px-0 my-4 cardGame",
+      classRelativeCard: "relativeCard",
+      classImgCard: "imgCard",
+      classTitleGame: "titleGame",
+      listTitleGame: "notDisplay",
+      classRow: "row mx-0 py-5 mt-4"
+    });
+  }
+
+  displayLine() {
+    this.setState({
+      classCardGame: "col-12 py-1 border-bottom listCardGame",
+      classRelativeCard: "notDisplay",
+      classImgCard: "notDisplay",
+      classTitleGame: "notDisplay",
+      listTitleGame: "ml-5 text-white titleWrap",
+      classRow: "row px-5 mx-0 pt-5 mt-4 paddingBottom"
+    });
+  }
+
+  render() {
+    return (
+      <div className="col-12 homePage px-0">
+        <section className="row mx-0">
+          <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true}>
+            <div>
+              <img src="index.jpeg" className="imgCarousel" />
+              <p className="legend">Legend 1</p>
+            </div>
+            <div>
+              <img src="index.jpeg" className="imgCarousel" />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src="index.jpeg" className="imgCarousel" />
+              <p className="legend">Legend 3</p>
+            </div>
+          </Carousel>
+        </section>
+        <Filter />
+        <section className="row mx-0">
+          <div className="col-12 layoutOrganisation">
+            <div className="col-sm-6 col-xl-10 py-4 border-right">
+              <span className="sectionTitle text-white">
+                <u>Games which may interest you</u>
+              </span>
+            </div>
+            <div className="col d-flex justify-content-center pt-4">
+              <img
+                src="card.png"
+                className="btnList mr-4"
+                onClick={() => this.displayCard()}
+              />
+              <img
+                src="line.png"
+                className="btnList"
+                onClick={() => this.displayLine()}
+              />
+            </div>
+          </div>
+        </section>
+        <section className={this.state.classRow}>
+          <div className={this.state.classCardGame}>
+            <div className={this.state.classRelativeCard}>
+              <img src="index.jpeg" className={this.state.classImgCard} />
+              <span className={this.state.classTitleGame}>
+                Metal gear solid V
+              </span>
+            </div>
+            <span className={this.state.listTitleGame}>title</span>
+          </div>
+          <div className={this.state.classCardGame}>
+            <div className={this.state.classRelativeCard}>
+              <img src="index.jpeg" className={this.state.classImgCard} />
+              <span className={this.state.classTitleGame}>
+                Metal gear solid V
+              </span>
+            </div>
+            <span className={this.state.listTitleGame}>title</span>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 export default Homepage;
